483514f03b5900b61f70f3d2ff30f7e2326c8d62
0dot77 <yty0706@gmail.com>
2022-06-18 18:28:28 +0900

    fix: body overflow

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -6,8 +6,8 @@ import TreeMain from '../components/TreeMain';
 
 const Layout = styled.div`
   display: grid;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background: ${(props) => props.theme.backgroundColor};
   grid-template-columns: repeat(4, 1fr);
   justify-items: center;
