0b5824cb5d4ac134244bef46a131777a78de5fe8
0dot77 <yty0706@gmail.com>
2022-06-30 20:53:07 +0900

    feat: web-gl 제거 밎, 전시 준비 완료

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RealTimeWishes from './RealTimeWishes';
 import Interviews from './Interviews';
 import WishAndTemple from './WishAndTemple';
-import WebGame from './WebGame';
 import Main from './Main';
 import Exhibition from './Exhibition';
 
@@ -15,7 +14,6 @@ function Router() {
           <Route path="/realtime-wishes" element={<RealTimeWishes />}></Route>
           <Route path="/interviews" element={<Interviews />}></Route>
           <Route path="/wish-and-temple" element={<WishAndTemple />}></Route>
-          <Route path="/web-game" element={<WebGame />}></Route>
         </Route>
         <Route path="/exhibition" element={<Exhibition />}>
           <Route path="/exhibition/realtime-wishes" element={<RealTimeWishes />}></Route>
