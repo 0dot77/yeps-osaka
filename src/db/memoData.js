98d659b790f661b6cd28b95b36dc7b493e9826a1
0dot77 <yty0706@gmail.com>
2022-07-01 12:19:59 +0900

    fix: url 주소 수정

--- src/db/memoData.js
+++ src/db/memoData.js
@@ -1,3 +1,14 @@
-const memoData = ['https://www.youtube.com/embed/HIO3U52NhS0', 'https://www.youtube.com/embed/c-czH4U4BHE'];
+const memoData = [
+  'https://www.youtube.com/embed/HIO3U52NhS0',
+  'https://www.youtube.com/embed/c-czH4U4BHE',
+  'https://www.youtube.com/embed/PLBYIZgVDwE',
+  'https://www.youtube.com/embed/oaUnRifSb-Y',
+  'https://www.youtube.com/embed/XB75u_7udgw',
+  'https://www.youtube.com/embed/zYhJYKVAW6o',
+  'https://www.youtube.com/embed/Gt_GjH-b43w',
+  'https://www.youtube.com/embed/ldG6V8LbtQU',
+  'https://www.youtube.com/embed/5ET961wYeW4',
+  'https://www.youtube.com/embed/LyOALgNUdQQ',
+];
 
 export default memoData;
